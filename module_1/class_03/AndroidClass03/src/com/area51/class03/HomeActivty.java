a324453ceda4d5b9d7f2563478d628aabcaea86e
Eduardo J.  Medina Alfaro <emedinaa@gmail.com>
2014-05-17 12:38:15 -0500

    class 03

---
+++ module_1/class_03/AndroidClass03/src/com/area51/class03/HomeActivty.java
@@ -0,0 +1,16 @@
+package com.area51.class03;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivty extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_home);
+		setContentView(R.layout.layout_p_h);
+	}
+
+}
